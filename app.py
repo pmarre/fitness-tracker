c22640519d56ee5dd78e86596e66fde7a148c013
Patrick Marre <pmarre40@gmail.com>
2020-06-19 11:37:00 -0700

    connected to db, validated email & pwd

--- app.py
+++ app.py
@@ -19,6 +19,18 @@ def login():
     return render_template("login.html", user=mongo.db.current_users.find())
 
 
+@app.route('/validate_login', methods=['POST'])
+def validate_login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if mongo.db.current_users.find_one({'email': email, 'password': password}) != None:
+        print('success')
+        return redirect(url_for('login'))
+    else:
+        print('failed, no such email or password')
+        return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
     # for local deployment:
     app.run(debug=True)
