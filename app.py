761fd77c4cb8ec89317a49c78da2fd657cda863e
Patrick Marre <pmarre40@gmail.com>
2020-07-16 17:21:56 -0700

    added addworkout page and connected to db

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = SECRET_KEY
 app.config["MONGO-DBNAME"] = "fitnessDB"
 app.config["MONGO_URI"] = MONGO_URI
 
@@ -38,7 +38,21 @@ def validate_login():
 
 @app.route('/dashboard/<user_id>', methods=['POST', 'GET'])
 def dashboard(user_id):
-    return render_template("dashboard.html", user=mongo.db.current_users.find_one({'_id': ObjectId(user_id)}))
+    return render_template("dashboard.html", user=mongo.db.current_users.find_one({'_id': ObjectId(user_id)}), workouts=mongo.db.workouts.find({'user_id': user_id}))
+
+
+@app.route('/addworkout/<user_id>', methods=['POST', 'GET'])
+def addworkout(user_id):
+    return render_template("addworkout.html", user_id=user_id)
+
+
+@app.route('/insert_workout', methods=['GET', 'POST'])
+def insert_workout():
+    workout = {'workout_type': request.form.get(
+        'workout-type'), 'workout_notes': request.form.get('workout-notes'), 'user_id': request.form.get('user_id')}
+    mongo.db.workouts.insert_one(workout)
+    id = request.form.get("user_id")
+    return redirect(url_for('dashboard', user_id=id))
 
 
 if __name__ == '__main__':
