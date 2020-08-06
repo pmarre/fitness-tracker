3b940f8b2f7d071aba989311d1fba996c3ba3e16
Patrick Marre <pmarre40@gmail.com>
2020-08-06 14:22:44 -0700

    Added edit profile page, fixed signup page error

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ def login():
 def validate_login():
     email = request.form.get("email")
     password = request.form.get("password")
-    session.pop('user_email', None)
+    session.pop('user_id', None)
     if mongo.db.current_users.find_one({'email': email, 'password': password}) != None:
         user = mongo.db.current_users.find_one({'email': email})
         user_id = user['_id']
-        session['user_email'] = email
+        session['user_id'] = str(user_id)
         return redirect(url_for('dashboard', user_id=user_id))
     else:
         flash('Login unsuccessful')
@@ -39,7 +39,7 @@ def validate_login():
 
 @app.route('/logout')
 def logout():
-    session.pop('user_email', None)
+    session.pop('user_id', None)
     return render_template('login.html')
 
 
@@ -54,13 +54,14 @@ def dashboard(user_id):
     # for x in recent_workouts_dict:
     #     print(x)
     user = mongo.db.current_users.find_one({'_id': ObjectId(user_id)})
-    count_workouts = mongo.db.workouts.find({'user_id': user_id}).count()
+    count_workouts = mongo.db.workouts.find(
+        {'user_id': user_id}).count()
 
     if user == None:
         return redirect(url_for("login"))
 
-    if session.get('user_email'):
-        if session['user_email'] == user['email']:
+    if session.get('user_id'):
+        if session['user_id'] == str(user['_id']):
             workout_dict = mongo.db.workouts.find(
                 {'user_id': user_id}).sort([('workout_date', -1)])
             recent_workout = mongo.db.workouts.find(
@@ -135,16 +136,20 @@ def sign_up_page():
 
     if password_1 != password_2:
         print("passwords don't match")
+        # add flash message
         return render_template('sign-up.html')
     elif mongo.db.current_users.find_one({'email': email}) != None:
+        # add flash message
         print("email already exists")
         return render_template('sign-up.html')
     else:
+        session.pop('user_id', None)
         mongo.db.current_users.insert_one(
             {'first_name': f_name, 'last_name': l_name, 'email': email, 'password': password_1, 'profile_image': new_name})
         if mongo.db.current_users.find_one({'email': email}) != None:
             user = mongo.db.current_users.find_one({'email': email})
             user_id = user['_id']
+            session['user_id'] = str(user_id)
             return redirect(url_for('dashboard', user_id=user_id))
 
 
@@ -206,10 +211,35 @@ def update_workout(workout_id):
     return redirect(url_for('dashboard', user_id=id))
 
 
+@app.route('/edit_profile/<user_id>')
+def edit_profile(user_id):
+    user = mongo.db.current_users.find_one({"_id": ObjectId(user_id)})
+    return render_template('editprofile.html', user=user)
+
+
+@app.route('/update_profile/<user_id>', methods=['GET', 'POST'])
+def update_profile(user_id):
+    user = mongo.db.current_users.find_one({"_id": ObjectId(user_id)})
+    if request.files['profile_image_update'].filename == '':
+        img = user['profile_image']
+    else:
+        updated_img = request.files['profile_image_update']
+        img = uuid.uuid1().hex
+        mongo.save_file(img, updated_img)
+
+    mongo.db.current_users.update({"_id": ObjectId(user_id)}, {
+        'first_name': request.form.get('first_name'),
+        'last_name': request.form.get('last_name'),
+        'email': request.form.get('email'),
+        'password': request.form.get('password'),
+        'profile_image': img})
+    return redirect(url_for('dashboard', user_id=user_id))
+
+
 if __name__ == '__main__':
     # for local deployment:
-    # app.run(debug=True)
+    app.run(debug=True)
 
     # for deployment to Heroku:
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')), debug=True)
+    # app.run(host=os.environ.get('IP'),
+#            port=int(os.environ.get('PORT')), debug=True)
