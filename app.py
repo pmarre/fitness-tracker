8a2af23ed4ac54c16a90aa4104c1a251fa1c5df4
Patrick Marre <pmarre40@gmail.com>
2020-08-20 13:01:15 -0700

    added env vars

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ import uuid
 import datetime
 
 # # For Heroku, comment out for local
-# MONGO_URI = os.environ.get('MONGO_URI')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# DB_NAME = os.environ.get('DB_NAME')
+MONGO_URI = os.environ.get('MONGO_URI')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DB_NAME = os.environ.get('DB_NAME')
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
