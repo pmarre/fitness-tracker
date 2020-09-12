cd5044d9649cb0fec8760928fa685f9d69860a29
Patrick Marre <pmarre40@gmail.com>
2020-09-12 09:00:06 -0700

    added in comments and cleaned up code

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-import datetime
+"""
+This is the app.py file that makes FitnessTracker function
+"""
+import os
 import uuid
 from flask_bcrypt import Bcrypt
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 from flask import Flask, flash, render_template, redirect, request, url_for, session
-import os
+from fitness_tracker_config import *
 
 # # For Heroku, comment out for local
-MONGO_URI = os.environ.get('MONGO_URI')
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DB_NAME = os.environ.get('DB_NAME')
+# MONGO_URI = os.environ.get('MONGO_URI')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# DB_NAME = os.environ.get('DB_NAME')
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -26,6 +29,9 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/sign-up', methods=['POST', 'GET'])
 def sign_up_page():
+    """
+    This function validates the signup form
+    """
     session.clear()
 
     if request.method == "POST":
@@ -43,18 +49,19 @@ def sign_up_page():
         else:
             new_name = None
 
-        if password_1 != password_2:
+        if password_1 is not password_2:
             flash('Passwords do not match')
             return render_template('sign-up.html')
-        elif mongo.db.current_users.find_one({'email': email}) != None:
+        elif mongo.db.current_users.find_one({'email': email}) is not None:
             flash('Email already exists')
             return render_template('sign-up.html')
         else:
             session.pop('user_id', None)
             pw_hash = bcrypt.generate_password_hash(password_1)
             mongo.db.current_users.insert_one(
-                {'first_name': f_name, 'last_name': l_name, 'email': email, 'password': pw_hash, 'profile_image': new_name})
-            if mongo.db.current_users.find_one({'email': email}) != None:
+                {'first_name': f_name, 'last_name': l_name, 'email': email,
+                 'password': pw_hash, 'profile_image': new_name})
+            if mongo.db.current_users.find_one({'email': email}) is not None:
                 user = mongo.db.current_users.find_one({'email': email})
                 user_id = user['_id']
                 session['user_id'] = str(user_id)
@@ -66,6 +73,7 @@ def sign_up_page():
 
 @app.route('/login')
 def login():
+    """ Renders Login Template """
     return render_template("login.html", user=mongo.db.current_users.find())
 
 ## VALIDATE USER LOGIN ##
@@ -73,13 +81,14 @@ def login():
 
 @app.route('/validate_login', methods=['POST', 'GET'])
 def validate_login():
+    """ Validates Login """
     if request.method == "POST":
         email = request.form.get("email").lower()
         password = request.form.get("password")
         user = mongo.db.current_users.find_one({'email': email})
         print(user)
         session.pop('user_id', None)
-        if user != None:
+        if user is not None:
             if bcrypt.check_password_hash(user['password'], password):
                 user_id = user['_id']
                 session['user_id'] = str(user_id)
@@ -97,6 +106,7 @@ def validate_login():
 
 @app.route('/logout')
 def logout():
+    """ Logs user out of site """
     session.pop('user_id', None)
     return render_template('login.html')
 
@@ -105,11 +115,12 @@ def logout():
 
 @app.route('/dashboard/<user_id>')
 def dashboard(user_id):
+    """ Renders the users dashboard to show all workouts """
     user = mongo.db.current_users.find_one({'_id': ObjectId(user_id)})
     count_workouts = mongo.db.workouts.find(
         {'user_id': user_id}).count()
 
-    if user == None:
+    if user is None:
         return redirect(url_for("login"))
 
     if session.get('user_id'):
@@ -122,7 +133,9 @@ def dashboard(user_id):
                 workouts = None
             else:
                 workouts = workout_dict
-            return render_template("dashboard.html", user=mongo.db.current_users.find_one({'_id': ObjectId(user_id)}), workouts=workouts, recent=recent_workout, count=count_workouts)
+            return render_template("dashboard.html", user=mongo.db.current_users.find_one(
+                {'_id': ObjectId(user_id)}), workouts=workouts,
+                recent=recent_workout, count=count_workouts)
         else:
             return redirect(url_for("login"))
     return redirect(url_for("login"))
@@ -132,25 +145,29 @@ def dashboard(user_id):
 
 @app.route('/addworkout/<user_id>')
 def addworkout(user_id):
-    return render_template("addworkout.html", user_id=user_id, user=mongo.db.current_users.find_one({'_id': ObjectId(user_id)}))
+    """ Renders add workout form template so user can add a new workout """
+    return render_template("addworkout.html", user_id=user_id,
+                           user=mongo.db.current_users.find_one({'_id': ObjectId(user_id)}))
 
 ## INSERT WORKOUT TO MONGO ##
 
 
 @app.route('/insert_workout', methods=['GET', 'POST'])
 def insert_workout():
+    """ Inserts the newly added workout to Mongo """
     if request.method == "POST":
-        h = request.form.get('workout-duration-h')
-        m = request.form.get('workout-duration-m')
-        s = request.form.get('workout-duration-s')
-        workout_duration = str(h) + 'h ' + str(m) + 'm ' + str(s) + 's'
+        hour = request.form.get('workout-duration-h')
+        minute = request.form.get('workout-duration-m')
+        second = request.form.get('workout-duration-s')
+        workout_duration = str(hour) + 'h ' + \
+            str(minute) + 'm ' + str(second) + 's'
 
         unit = request.form.get('workout-distance-units')
         distance = request.form.get('workout-distance')
-        if (distance == None):
-            d = '0'
+        if distance is None:
+            distance = '0'
         else:
-            d = str(distance)
+            distance = str(distance)
 
         uploaded_image = request.files['workout_image']
         if uploaded_image.filename == '':
@@ -162,11 +179,11 @@ def insert_workout():
             mongo.save_file(new_name, workout_image)
 
         workout = {
-            'workout_duration_h': h,
-            'workout_duration_m': m,
-            'workout_duration_s': s,
+            'workout_duration_h': hour,
+            'workout_duration_m': minute,
+            'workout_duration_s': second,
             'workout_duration': workout_duration,
-            'workout_distance': d,
+            'workout_distance': distance,
             'workout_distance_metric': unit,
             'workout_type': request.form.get('workout-type'),
             'workout_title': request.form.get('workout-title'),
@@ -175,8 +192,8 @@ def insert_workout():
             'user_id': request.form.get('user_id'),
             'workout_image': new_name}
         mongo.db.workouts.insert_one(workout)
-        id = request.form.get("user_id")
-        return redirect(url_for('dashboard', user_id=id))
+        user_id = request.form.get("user_id")
+        return redirect(url_for('dashboard', user_id=user_id))
     return redirect(url_for('addworkout'))
 
 ## UPLOAD IMAGE TO MONGO ##
@@ -184,6 +201,7 @@ def insert_workout():
 
 @app.route('/file/<filename>')
 def file(filename):
+    """ Sends file to Mongo """
     return mongo.send_file(filename)
 
 ## DELETE WORKOUT FROM MONGO ##
@@ -191,6 +209,7 @@ def file(filename):
 
 @app.route('/delete_workout/<workout_id>')
 def delete_workout(workout_id):
+    """ Deletes workout from Mongo """
     workout = mongo.db.workouts.find_one({"_id": ObjectId(workout_id)})
     mongo.db.workouts.remove({'_id': ObjectId(workout_id)})
     return redirect(url_for('dashboard', user_id=workout['user_id']))
@@ -200,26 +219,32 @@ def delete_workout(workout_id):
 
 @app.route('/edit_workout/<workout_id>')
 def edit_workout(workout_id):
+    """ Edits workout in Mongo """
     the_workout = mongo.db.workouts.find_one({"_id": ObjectId(workout_id)})
-    return render_template('editworkout.html', workout=the_workout, user=mongo.db.current_users.find_one({'_id': ObjectId(the_workout['user_id'])}))
+    return render_template('editworkout.html',
+                           workout=the_workout,
+                           user=mongo.db.current_users.find_one(
+                               {'_id': ObjectId(the_workout['user_id'])}))
 
 ## UPDATE WORKOUT IN MONGO ##
 
 
 @app.route('/update_workout/<workout_id>', methods=['GET', 'POST'])
 def update_workout(workout_id):
+    """ Updates workout in Mongo """
     if request.method == "POST":
-        h = request.form.get('workout-duration-h')
-        m = request.form.get('workout-duration-m')
-        s = request.form.get('workout-duration-s')
-        workout_duration = str(h) + 'h ' + str(m) + 'm ' + str(s) + 's'
+        hour = request.form.get('workout-duration-h')
+        minute = request.form.get('workout-duration-m')
+        second = request.form.get('workout-duration-s')
+        workout_duration = str(hour) + 'h ' + \
+            str(minute) + 'm ' + str(second) + 's'
 
         unit = request.form.get('workout-distance-units')
         distance = request.form.get('workout-distance')
-        if (distance == None):
-            d = '0'
+        if distance is None:
+            distance = '0'
         else:
-            d = str(distance)
+            distance = str(distance)
 
         workout = mongo.db.workouts.find_one({"_id": ObjectId(workout_id)})
 
@@ -231,11 +256,11 @@ def update_workout(workout_id):
             mongo.save_file(img, updated_img)
 
         mongo.db.workouts.update({"_id": ObjectId(workout_id)}, {
-            'workout_duration_h': h,
-            'workout_duration_m': m,
-            'workout_duration_s': s,
+            'workout_duration_h': hour,
+            'workout_duration_m': minute,
+            'workout_duration_s': second,
             'workout_duration': workout_duration,
-            'workout_distance': d,
+            'workout_distance': distance,
             'workout_distance_metric': unit,
             'workout_type': request.form.get('workout-type'),
             'workout_title': request.form.get('workout-title'),
@@ -243,8 +268,8 @@ def update_workout(workout_id):
             'workout_date': request.form.get('workout-date'),
             'user_id': request.form.get('user_id'),
             'workout_image': img})
-        id = request.form.get("user_id")
-        return redirect(url_for('dashboard', user_id=id))
+        user_id = request.form.get("user_id")
+        return redirect(url_for('dashboard', user_id=user_id))
     return redirect(url_for('update_workout', workout_id=workout_id))
 
 ## EDIT PROFILE PAGE ##
@@ -252,6 +277,7 @@ def update_workout(workout_id):
 
 @app.route('/edit_profile/<user_id>')
 def edit_profile(user_id):
+    """ Edit profile information """
     user = mongo.db.current_users.find_one({"_id": ObjectId(user_id)})
     return render_template('editprofile.html', user=user)
 
@@ -260,6 +286,7 @@ def edit_profile(user_id):
 
 @app.route('/update_profile/<user_id>', methods=['GET', 'POST'])
 def update_profile(user_id):
+    """ Update profile in Mongo """
     if request.method == "POST":
         user = mongo.db.current_users.find_one({"_id": ObjectId(user_id)})
         if request.files['profile_image_update'].filename == '':
@@ -287,6 +314,7 @@ def update_profile(user_id):
 
 @app.route('/delete_profile/<user_id>')
 def delete_profile(user_id):
+    """ Delete profile from Mongo """
     mongo.db.current_users.remove({'_id': ObjectId(user_id)})
     return redirect(url_for('sign_up_page'))
 
@@ -295,6 +323,7 @@ def delete_profile(user_id):
 
 @app.route('/about')
 def about():
+    """ Check if user is logged in on about page to render proper nav """
     if session.get('user_id'):
         print('logged in')
         user_id = session['user_id']
@@ -306,8 +335,8 @@ def about():
 
 if __name__ == '__main__':
     # for local deployment:
-    # app.run(debug=True)
+    app.run(debug=True)
 
     # for deployment to Heroku:
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')), debug=False)
+    # app.run(host=os.environ.get('IP'),
+    #         port=int(os.environ.get('PORT')), debug=False)
--- static/js/workouts.js
+++ static/js/workouts.js
@@ -3,9 +3,21 @@ showDistanceForm = () => {
   let sport = $('#workout-type option:selected');
   if (sport.val() == 'Weight Training' || sport.val() == 'Other') {
     $('#workout-distance-container').hide();
-    console.log(sport.val());
   } else {
     $('#workout-distance-container').show();
-    console.log(sport.val());
   }
 };
+
+// $(document).ready(function () {
+//   $('.submit-form-btn').click(function (e) {
+//     if ($('input:invalid')) {
+//       e.preventDefault();
+//       $('input:invalid').addClass('error');
+//       console.log('');
+//     } else {
+//       $('input:valid').removeClass('error');
+//       console.log('no errors');
+//       $('.add-workout-form').submit();
+//     }
+//   });
+// });
